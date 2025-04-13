b17768181431c75ac6ea0b6ff20f0625898c2c58
gabrielemasciulli <gmasciu@gmail.com>
2025-04-13 12:18:27 +0200

    Added 2 dropout layers: - One after the embedding layer - One before the last FC linear layer

--- LM/part_1A/model.py
+++ LM/part_1A/model.py
@@ -5,8 +5,8 @@ from utils import DEVICE
 
 
 class LM_LSTM(nn.Module):
-    def __init__(self, emb_size, hidden_size, vocab_len, pad_index=0, out_dropout=0.1,
-                 emb_dropout=0.1, n_layers=1, pretrained_weights=None):
+    def __init__(self, emb_size, hidden_size, vocab_len, pad_index=0, emb_dropout_rate=0.1,
+                 out_dropout_rate=0.1, n_layers=1, pretrained_weights=None):
         super(LM_LSTM, self).__init__()
         self.hidden_size = hidden_size
         self.n_layers = n_layers
@@ -21,15 +21,20 @@ class LM_LSTM(nn.Module):
             self.embedding = nn.Embedding(
                 vocab_len, emb_size, padding_idx=pad_index)
 
+        self.emb_dropout = nn.Dropout(emb_dropout_rate)
+
         # LSTM layer
         self.lstm = nn.LSTM(emb_size, hidden_size,
                             num_layers=n_layers, batch_first=True)
 
+        self.out_dropout = nn.Dropout(out_dropout_rate)
         # Output layer (fully connected)
         self.output = nn.Linear(hidden_size, vocab_len)
 
     def forward(self, input_sequence):
         emb = self.embedding(input_sequence)
+        emb = self.emb_dropout(emb)
         rnn_out, _ = self.lstm(emb)
+        rnn_out = self.out_dropout(rnn_out)
         output = self.output(rnn_out).permute(0, 2, 1)
         return output
